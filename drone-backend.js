903a3ad785eb503850c104a2729bb1a7add771f0
foo <foo@bar.baz>
2015-08-14 15:39:44 +0100

    Modified code based on drone testing

--- drone-backend.js
+++ drone-backend.js
@@ -11,46 +11,54 @@ Cylon.robot()
         driver: "ardrone",
         connection: "ardrone"
     })
+    .device("nav", {
+        driver: "ardrone-nav",
+        connection: "ardrone"
+    })
     .on("ready", fly);
     
 // Fly the bot
-.device("nav", {
-    driver: "ardrone-nav",
-connection: "ardrone"
-})
 
-bot.nav.on("navdata", function(data) {
-    console.log(data);
-});
 
 
 function fly(robot) {
-    bot = robot
+    bot = robot;
+
+    bot.nav.on("navdata", function(data) {
+        console.log(data);
+    });
 
     bot.drone.disableEmergency();
     bot.drone.ftrim();
 
     bot.drone.takeoff();
 
-    bot.drone.foward(0.2);
-    after(2*1000, function() {
+    after(8 * 1000, function () {
+        bot.drone.forward(0.2);
+    });
+    after(10 * 1000, function () {
         bot.drone.forward(0);
+        bot.drone.hover();
+    });
+    after(11 * 1000, function () {
+        bot.drone.left(0.2);
+    });
+    after(13 * 1000, function () {
+        bot.drone.left(0);
+        bot.drone.hover();
+    });
+    after(14 * 1000, function () {
+        bot.drone.back(0.2);
+    });
+    after(16 * 1000, function () {
+        bot.drone.back(0);
+        bot.drone.hover();
     });
 
-    bot.drone.left(0.2);
-    after(5*1000,function() {
-        bot.drone.left(0)
-    });
-
-    bot.drone.up(0.3);
-    after(7*1000,function(){
-        bot.drone.up(0)
-    });
-
-    after(10*1000, function() {
+    after(17 * 1000, function () {
         bot.drone.land();
     });
-    after(15*1000, function() {
+    after(22 * 1000, function () {
         bot.drone.stop();
     });
 
